0c3709eff73b2b6c4e573b6699083deb764cae44
Ian <82505982+ian-pge@users.noreply.github.com>
2025-04-13 13:59:23 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -63,11 +63,29 @@
   };
 
   programs.fuse.userAllowOther = true;
-  home-manager = {
-    extraSpecialArgs = {inherit inputs;};
-    users = {
-      "vimjoyer" = import ./home.nix;
-    };
+  #home-manager = {
+    #extraSpecialArgs = {inherit inputs;};
+    #users = {
+      #"vimjoyer" = import ./home.nix;
+    #};
+  #};
+
+  nixpkgs.config.allowUnfree = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  programs.dconf.enable = true;
 
 }
