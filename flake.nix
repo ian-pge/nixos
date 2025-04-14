7cf966fdf6286e0b1efbe429f8eb023475db5926
Ian <82505982+ian-pge@users.noreply.github.com>
2025-04-14 11:22:00 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,10 +13,10 @@
       url = "github:nix-community/impermanence";
     };
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {nixpkgs, ...} @ inputs:
@@ -29,7 +29,7 @@
 
         ./configuration.nix
               
-        # inputs.home-manager.nixosModules.default
+        inputs.home-manager.nixosModules.default
         inputs.impermanence.nixosModules.impermanence
       ];
     };
