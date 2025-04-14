6e26fa0abf729e707ffc26accd186201bbeb2104
Ian <82505982+ian-pge@users.noreply.github.com>
2025-04-14 13:40:30 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,12 +1,15 @@
 { pkgs, inputs, ... }:
 
-{ 
+{
+  # Import impermanence integration for home-manager.
   imports = [
     inputs.impermanence.nixosModules.home-manager.impermanence
   ];
 
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  # Set the home-manager state version (do not change unless you know why).
+  home.stateVersion = "24.11";
 
+  # Home persistence configuration: list all directories and files you want to persist.
   home.persistence."/persist/home" = {
     directories = [
       "Downloads"
@@ -25,21 +28,19 @@
         method = "symlink";
       }
     ];
-    files = [
-      ".screenrc"
-    ];
+    files = [ ".screenrc" ];
     allowOther = true;
   };
-  {
-  # Enable dconf to manage GNOME settings
+
+  # dconf settings for GNOME: configuring interface colors and theme.
   dconf.settings = {
     "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";  # Preferred color scheme
-      gtk-theme = "Adwaita-dark";    # Set GTK theme to Adwaita-dark
+      color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
     };
   };
 
-  # Configure GTK settings
+  # GTK settings: enable GTK, set dark theme and proper icon/cursor themes.
   gtk = {
     enable = true;
     theme = {
