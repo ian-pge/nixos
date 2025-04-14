76b8e438c3d59aa4d55f37c4a9a599a546cad7f0
Ian <82505982+ian-pge@users.noreply.github.com>
2025-04-14 11:26:11 +0000

    Update home.nix

--- home.nix
+++ home.nix
@@ -30,4 +30,30 @@
     ];
     allowOther = true;
   };
+  {
+  # Enable dconf to manage GNOME settings
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";  # Preferred color scheme
+      gtk-theme = "Adwaita-dark";    # Set GTK theme to Adwaita-dark
+    };
+  };
+
+  # Configure GTK settings
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
+    cursorTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
+  };
 }
