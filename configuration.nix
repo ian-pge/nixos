bf8f1d74a6617eca5a9b3c7c5d4c93b80f41d5e3
Ian <82505982+ian-pge@users.noreply.github.com>
2025-04-14 10:47:41 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
 
   system.stateVersion = "23.05"; # Did you read the comment?
 
-    users.users."ian " = {
+    users.users."ian" = {
     isNormalUser = true;
     initialPassword = "ianbage";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
