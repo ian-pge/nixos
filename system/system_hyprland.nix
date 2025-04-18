7cead09557f65bfd3ba1bb6a1de987b9551953c7
ian <ian.page38@gmail.com>
2025-04-18 20:15:49 +0200

    Update system_hyprland.nix

--- system/system_hyprland.nix
+++ system/system_hyprland.nix
@@ -50,7 +50,13 @@
         };
     };
 
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services.login.enableGnomeKeyring = true;
+    security.pam.services.hyprlock.enableGnomeKeyring = true;
+
     networking.wireless.iwd.enable = true;
+    networking.networkmanager.enable = true;
+    networking.networkmanager.wifi.backend = "iwd";
 
     programs.hyprland.enable = true;
     # xdg.portal = {
