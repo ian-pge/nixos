0d2047f707035e6a624105029df977645c4d7b8a
ian <ian.page38@gmail.com>
2025-04-19 20:54:38 +0200

    Update system_hyprland.nix

--- system/system_hyprland.nix
+++ system/system_hyprland.nix
@@ -68,12 +68,6 @@
 
         uwsm = {
             enable = true;
-            waylandCompositors = {
-              hyprland = {
-                prettyName = "Hyprland";
-                comment = "Hyprland compositor managed by UWSM";
-                binPath = "${pkgs.hyprland}/bin/Hyprland";
-              };
             };
           };
 
