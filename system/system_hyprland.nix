ab82d2b499d3e3561b5c21d8139a82636d934514
ian <ian.page38@gmail.com>
2025-04-19 20:51:33 +0200

    Update system_hyprland.nix

--- system/system_hyprland.nix
+++ system/system_hyprland.nix
@@ -98,7 +98,7 @@
         settings = {
           initial_session = {
             # fallback if tuigreet fails (TTY autologin)
-            command = "${pkgs.greety}/bin/agreety --cmd ${pkgs.bash}/bin/bash";
+            command = "${pkgs.greetd}/bin/greetd --cmd ${pkgs.bash}/bin/bash";
             user    = "root";
           };
 
