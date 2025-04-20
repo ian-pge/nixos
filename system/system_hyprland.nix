9f812046cb9068ecbd97c86842601058fed78ca1
ian <ian.page38@gmail.com>
2025-04-20 20:03:42 +0200

    Update system_hyprland.nix

--- system/system_hyprland.nix
+++ system/system_hyprland.nix
@@ -90,7 +90,7 @@
                 --asterisks \
                 --sessions /run/current-system/sw/share/wayland-sessions:/run/current-system/sw/share/xsessions \
                 --cmd Hyprland";          # start Hyprland after auth
-            user = "greetd";           # unprivileged greeter user
+            user = "greeter";           # unprivileged greeter user
           };
         };
       };
