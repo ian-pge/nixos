5429f6a76c8e30a7b5aa1c6050e39f2a0369b9bd
ian <ian.page38@gmail.com>
2025-04-21 10:18:34 +0200

    Update system_hyprland.nix

--- system/system_hyprland.nix
+++ system/system_hyprland.nix
@@ -78,12 +78,12 @@
     services.greetd = {
         enable = true;                  # start greetd daemon
         vt     = 1;                     # run on tty1
-        settings = {S
+        settings = {
           default_session = {
             # tuigreet binary from nixpkgs
             command = "${pkgs.greetd.tuigreet}/bin/tuigreet ---remember -width 40\
             --time\
-           S --asterisks --theme 'border=magenta;prompt=green;time=yellow;button=yellow;container=black;input=cyan'";
+            --asterisks --theme 'border=magenta;prompt=green;time=yellow;button=yellow;container=black;input=cyan'";
             user = "greeter";           # unprivileged greeter user
           };
         };
