c5f7bdfeeb5698f8b2df5069a87df4046c17b40d
ian <ian.page38@gmail.com>
2025-04-22 11:39:48 +0200

    Update home_hyprland.nix

--- home_manager/home_hyprland.nix
+++ home_manager/home_hyprland.nix
@@ -266,9 +266,6 @@
         LANG = "en_US.UTF-8";
         };
         enable = true;
-        fastSyntaxHighlighting.enable = true;
-        autosuggestion.enable = true;
-        # syntaxHighlighting.enable = true;
         completionInit = "autoload -Uz compinit && compinit";
         initExtra = lib.concatStringsSep "\n" [
               # 1. Syntax highlighting
