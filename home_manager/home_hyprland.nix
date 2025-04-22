fdc7c2d3a0591388dfe4ce1a797b0dde808dd63c
ian <ian.page38@gmail.com>
2025-04-22 13:27:34 +0200

    Update home_hyprland.nix

--- home_manager/home_hyprland.nix
+++ home_manager/home_hyprland.nix
@@ -37,7 +37,7 @@
         enable  = true;                   # turn on the hyprpaper service
         settings = {
           ipc      = "off";                # enable fast IPC mode for live changes
-          # preload  = [ "/home/you/Pictures/wall.png" ];   # images to load at startup
+          preload  = [ "/etc/nixos/material/wallpaper.png" ];   # images to load at startup
           wallpaper = [ ",/etc/nixos/material/wallpaper.png" ]; # apply to all monitors
         };
       };
