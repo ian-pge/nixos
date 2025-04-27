8d009c6ba6620a642dec20c2d6df8289088cd1da
ian <ian.page38@gmail.com>
2025-04-27 12:06:45 +0200

    Update home_hyprland.nix

--- home_manager/home_hyprland.nix
+++ home_manager/home_hyprland.nix
@@ -6,32 +6,32 @@
   ];
 
   # Enable dconf to manage GNOME settings
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";  # Preferred color scheme
-      gtk-theme = "Adwaita-dark";    # Set GTK theme to Adwaita-dark
-    };
-  };
+  # dconf.settings = {
+  #   "org/gnome/desktop/interface" = {
+  #     color-scheme = "prefer-dark";  # Preferred color scheme
+  #     gtk-theme = "Adwaita-dark";    # Set GTK theme to Adwaita-dark
+  #   };
+  # };
 
-  # Configure GTK settings
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-    iconTheme = {
-      name = "Adwaita";
-      package = pkgs.adwaita-icon-theme;
-    };
-    cursorTheme = {
-      name = "catppuccin-macchiato-dark-cursors";
-      package = pkgs.catppuccin-cursors.macchiatoDark;
-    };
+  # # Configure GTK settings
+  # gtk = {
+  #   enable = true;
+  #   theme = {
+  #     name = "Adwaita-dark";
+  #     package = pkgs.gnome-themes-extra;
+  #   };
+  #   iconTheme = {
+  #     name = "Adwaita";
+  #     package = pkgs.adwaita-icon-theme;
+  #   };
+  #   cursorTheme = {
+  #     name = "catppuccin-macchiato-dark-cursors";
+  #     package = pkgs.catppuccin-cursors.macchiatoDark;
+  #   };
 
-    # Tells GTK 3 to prefer a dark theme variant
-    gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-  };
+  #   # Tells GTK 3 to prefer a dark theme variant
+  #   gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
+  # };
 
   services.hyprpaper = {
         enable  = true;                   # turn on the hyprpaper service
