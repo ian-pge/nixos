7f8a7bd60040b00d55ec9526480d5dc8d1f53a98
ian <ian.page38@gmail.com>
2025-04-27 10:58:58 +0200

    Update home_hyprland.nix

--- home_manager/home_hyprland.nix
+++ home_manager/home_hyprland.nix
@@ -65,7 +65,7 @@
   };
   programs.helix = {
           enable = true;
-          theme = "catppuccin_macchiato";
+          theme = "catppuccin_Macchiato";
         };
 
   wayland.windowManager.hyprland = {
