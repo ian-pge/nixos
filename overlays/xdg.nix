3d610e36d5b7d5bb1095cd37ac3ccffe285bbf0f
ian <ian.page38@gmail.com>
2025-04-29 01:43:14 +0200

    Update xdg.nix

--- overlays/xdg.nix
+++ overlays/xdg.nix
@@ -11,7 +11,7 @@ in {
       rev = "v${newVersion}";
       # First build with lib.fakeSha256, copy the hash Nix prints,
       # then replace the line below.
-      hash = "12yy7f65zc2skxa7vf25da8653p94gll4jmjx4rj0wvb48yl2qd3";
+      hash = "sha256-12yy7f65zc2skxa7vf25da8653p94gll4jmjx4rj0wvb48yl2qd3";
     };
 
     # Meson / C project – nothing else to change.
