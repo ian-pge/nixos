13e6df324234b455ede4f01421cbbac099786e1f
ian <ian.page38@gmail.com>
2025-04-28 18:15:52 +0200

    Update nvidia.nix

--- system/hyprland/nvidia.nix
+++ system/hyprland/nvidia.nix
@@ -1,17 +1,18 @@
 {config, ...}: {
-  nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  hardware = {
+    # Enable OpenGL
+    opengl.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+    };
   };
 
   boot.kernelParams = ["nvidia-drm.modeset=1"];
-
-  # Enable OpenGL
-  hardware.opengl.enable = true;
 
   xserver = {
     enable = false;
