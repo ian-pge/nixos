20621b9a4a2ec18e5090081c8c4e8bda0c7ecd39
ian <ian.page38@gmail.com>
2025-04-28 13:38:12 +0200

    Update system.nix

--- system/system.nix
+++ system/system.nix
@@ -1,95 +1,20 @@
 {
   pkgs,
-  lib,
   inputs,
   ...
 }: {
   imports = [
-    ./hardware-configuration.nix
     (import ./disko.nix {device = "/dev/nvme0n1";})
+
     inputs.disko.nixosModules.default
     inputs.impermanence.nixosModules.impermanence
     inputs.home-manager.nixosModules.default
+
+    ./hardware-configuration.nix
+    ./config/boot.nix
+    ./config/persistance.nix
+    ./config/nixos_config.nix
   ];
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
-
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-      mkdir -p /btrfs_tmp/old_roots
-      timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-      mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
-
-    delete_subvolume_recursively() {
-      IFS=$'\n'
-      for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-        delete_subvolume_recursively "/btrfs_tmp/$i"
-      done
-      btrfs subvolume delete "$1"
-    }
-
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-      delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
-
-  fileSystems."/persist".neededForBoot = true;
-  environment.persistence."/persist/system" = {
-    hideMounts = true;
-    directories = [
-      "/etc/nixos"
-      "/var/log"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/var/lib/docker"
-      "/var/lib/NetworkManager"
-      "/var/lib/iwd"
-      "/var/cache/tuigreet"
-      "/etc/NetworkManager/system-connections"
-      {
-        directory = "/var/lib/colord";
-        user = "colord";
-        group = "colord";
-        mode = "u=rwx,g=rx,o=";
-      }
-    ];
-    files = [
-      "/etc/machine-id"
-      {
-        file = "/var/keys/secret_file";
-        parentDirectory = {mode = "u=rwx,g=,o=";};
-      }
-    ];
-  };
-
-  systemd.tmpfiles.rules = [
-    "d /persist/home 0777 root root -"
-    "d /persist/home/ian 0700 ian users -"
-  ];
-
-  system.stateVersion = "24.11";
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  nixpkgs.config.allowUnfree = true;
-
-  time.timeZone = "Europe/Paris";
-
-  users.users."ian" = {
-    isNormalUser = true;
-    initialPassword = "ianbage";
-    extraGroups = ["wheel" "docker" "video" "networkmanager"]; # Enable ‘sudo’ for the user.
-  };
 
   hardware = {
     keyboard.qmk.enable = true;
