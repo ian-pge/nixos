5d921bca1cd0689bca0db4c421e608125a68cf4a
ian <ian.page38@gmail.com>
2025-04-28 13:16:14 +0200

    cleaning files

--- system/system_gnome.nix
+++ system/system_gnome.nix
@@ -1,29 +1,31 @@
-{ config, inputs, ... }:
-
 {
-    home-manager = {
-        extraSpecialArgs = {inherit inputs;};
-        backupFileExtension = "backup";
-        users = {
-            "ian" = import ../home_manager/home_gnome.nix;
-        };
+  config,
+  inputs,
+  ...
+}: {
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    backupFileExtension = "backup";
+    users = {
+      "ian" = import ../home_manager/home_gnome.nix;
     };
+  };
 
-    hardware.nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
-        open = false;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
-    services = {
-        xserver = {
-            enable = true;
-            videoDrivers = ["nvidia"];
-            displayManager.gdm.enable = true;
-            desktopManager.gnome.enable = true;
-        };
+  services = {
+    xserver = {
+      enable = true;
+      videoDrivers = ["nvidia"];
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
     };
+  };
 }
--- system/system_hyprland.nix
+++ system/system_hyprland.nix
@@ -1,153 +1,152 @@
-{ config, pkgs, inputs, lib, ... }:
-
 {
-    imports =
-      [
-          inputs.stylix.nixosModules.stylix
-      ];
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}: {
+  imports = [
+    inputs.stylix.nixosModules.stylix
+  ];
 
-    home-manager = {
-        extraSpecialArgs = {inherit inputs;};
-        backupFileExtension = "backup";
-        users = {
-            "ian" = import ../home_manager/home_hyprland.nix;
-        };
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    backupFileExtension = "backup";
+    users = {
+      "ian" = import ../home_manager/home_hyprland.nix;
+    };
+  };
+
+  hardware = {
+    # Enable OpenGL
+    graphics.enable = true;
+
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
     };
 
-    hardware = {
-        # Enable OpenGL
-        graphics.enable = true;
-
-        bluetooth = {
-            enable = true;
-            powerOnBoot = true;
-        };
-
-        nvidia = {
-            modesetting.enable = true;
-            powerManagement.enable = true;
-            powerManagement.finegrained = false;
-            open = false;
-            nvidiaSettings = true;
-            package = config.boot.kernelPackages.nvidiaPackages.latest;
-            };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
+  };
 
-    boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+  boot.kernelParams = ["nvidia-drm.modeset=1"];
 
-    services = {
-
-        pipewire = {
-            wireplumber.enable = true;
-            enable = true;
-            alsa = {
-                enable = true;
-                support32Bit = true;
-            };
-            pulse.enable = true;
-            jack.enable = true;
-        };
-
-        xserver = {
-            enable = false;
-            videoDrivers = ["nvidia"];
-        };
-    };
-
-    services.gnome.gnome-keyring.enable = true;
-
-    security.pam.services.greetd.enableGnomeKeyring = true;
-    security.pam.services.hyprlock.enableGnomeKeyring = true;
-    security.pam.services.login.enableGnomeKeyring = true;
-
-    networking = {
-      useDHCP = false;             # NM will do DHCP itself
-      networkmanager = {
+  services = {
+    udisks2.enable = true;
+    gnome.gnome-keyring.enable = true;
+    pipewire = {
+      wireplumber.enable = true;
+      enable = true;
+      alsa = {
         enable = true;
-        # wifi.backend = "iwd";      # make NM talk to iwd instead of wpa_supplicant
+        support32Bit = true;
       };
+      pulse.enable = true;
+      jack.enable = true;
     };
-
-    fonts.packages = with pkgs; [
-      nerd-fonts.hack
-      nerd-fonts.ubuntu
-    ];
-
-    programs = {
-        hyprland = {
-            enable = true;
-            withUWSM = true;
-            xwayland.enable = true;
-        };
+    xserver = {
+      enable = false;
+      videoDrivers = ["nvidia"];
     };
-
-    services.greetd = {
-        enable = true;                  # start greetd daemon
-        vt     = 1;                     # run on tty1
-        settings = {
-          default_session = {
-            # tuigreet binary from nixpkgs
-            command =
-            lib.concatStringsSep " " [
-                "${pkgs.greetd.tuigreet}/bin/tuigreet"
-                "--remember"
-                "--width" "40"
-                "--time"
-                "--asterisks"
-                "--theme" "'border=magenta;prompt=green;time=yellow;container=black;input=cyan'"
-              ];
-            user = "greeter"; # unprivileged greeter user
-          };
+    greetd = {
+      enable = true; # start greetd daemon
+      vt = 1; # run on tty1
+      settings = {
+        default_session = {
+          # tuigreet binary from nixpkgs
+          command = lib.concatStringsSep " " [
+            "${pkgs.greetd.tuigreet}/bin/tuigreet"
+            "--remember"
+            "--width"
+            "40"
+            "--time"
+            "--asterisks"
+            "--theme"
+            "'border=magenta;prompt=green;time=yellow;container=black;input=cyan'"
+          ];
+          user = "greeter"; # unprivileged greeter user
         };
       };
+    };
+  };
 
-    services.udisks2.enable = true;
+  security.pam.services = {
+    greetd.enableGnomeKeyring = true;
+    hyprlock.enableGnomeKeyring = true;
+    enableGnomeKeyring = true;
+  };
 
-    stylix.enable = true;
-    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    stylix.autoEnable = false;
+  networking = {
+    useDHCP = false; # NM will do DHCP itself
+    networkmanager = {
+      enable = true;
+      # wifi.backend = "iwd";      # make NM talk to iwd instead of wpa_supplicant
+    };
+  };
 
-    stylix.fonts.monospace = {
-        name    = "Hack Nerd Font";
-        package = pkgs.nerd-fonts.hack;
-      };
+  fonts.packages = with pkgs; [
+    nerd-fonts.hack
+    nerd-fonts.ubuntu
+  ];
 
-    programs.zsh.enable = true;
-    users.defaultUserShell = pkgs.zsh;
+  programs = {
+    zsh.enable = true;
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+      xwayland.enable = true;
+    };
+  };
 
+  users.defaultUserShell = pkgs.zsh;
 
-    # Packages
-    environment.systemPackages = with pkgs; [
-        # hyprland
-        hypridle
-        hyprlock
-        hyprpaper
-        hyprpicker
-        hyprshot
-        hyprpolkitagent
-        hyprcursor
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+    autoEnable = false;
+    fonts.monospace = {
+      name = "Hack Nerd Font";
+      package = pkgs.nerd-fonts.hack;
+    };
+  };
 
-        bluetui
-        wl-clipboard
+  environment.systemPackages = with pkgs; [
+    # hyprland
+    hypridle
+    hyprlock
+    hyprpaper
+    hyprpicker
+    hyprshot
+    hyprpolkitagent
+    hyprcursor
 
-        mako
-        swayosd
-        waybar
-        udiskie
-        rofi-wayland
-        yazi
-        greetd.tuigreet
-        greetd.greetd
-        playerctl
-        vlc
-        htop
-        nvtopPackages.full
-        adwaita-icon-theme
-        catppuccin-cursors.macchiatoDark
-        oh-my-posh
-        nautilus
-        nmap
-        obs-studio
+    bluetui
+    wl-clipboard
 
-    ];
+    mako
+    swayosd
+    waybar
+    udiskie
+    rofi-wayland
+    yazi
+    greetd.tuigreet
+    greetd.greetd
+    playerctl
+    vlc
+    htop
+    nvtopPackages.full
+    adwaita-icon-theme
+    catppuccin-cursors.macchiatoDark
+    oh-my-posh
+    nautilus
+    nmap
+    obs-studio
+  ];
 }
