c0baa2c213dad94bccddcbaf393ce1b319074dc3
ian <ian.page38@gmail.com>
2025-04-28 19:13:45 +0200

    Update default.nix

--- home_manager/hyprland/default.nix
+++ home_manager/hyprland/default.nix
@@ -22,14 +22,6 @@
     nmap
   ];
 
-  # Enable dconf to manage GNOME settings
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark"; # Preferred color scheme
-      gtk-theme = "Adwaita-dark"; # Set GTK theme to Adwaita-dark
-    };
-  };
-
   services.mako.enable = true;
   services.swayosd.enable = true;
   services.udiskie.enable = true;
