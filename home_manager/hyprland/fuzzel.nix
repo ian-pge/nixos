99423ff73b176eeca08582bf7846d47e3a3c124e
ian <ian.page38@gmail.com>
2025-04-29 18:38:58 +0200

    Update fuzzel.nix

--- home_manager/hyprland/fuzzel.nix
+++ home_manager/hyprland/fuzzel.nix
@@ -6,12 +6,9 @@
         dpi-aware = "no";
         use-bold = "yes";
         font = lib.mkForce "Ubuntu Nerd Font:size=20";
-        keyboard-focus = "on-demand";
-        exit-on-keyboard-focus-loss = "no";
         layer = "overlay";
         icon-theme = "Adwaita";
-        horizontal-pad = "4000";
-        vertical-pad = "4000";
+        image-size-ratio = "1";
       };
       colors = {
         background = lib.mkForce "000000aa";
