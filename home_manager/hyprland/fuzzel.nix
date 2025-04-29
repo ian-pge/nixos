e2252aefd7adf886b9589aaa1b802690b6420abd
ian <ian.page38@gmail.com>
2025-04-29 18:47:34 +0200

    colors

--- home_manager/hyprland/fuzzel.nix
+++ home_manager/hyprland/fuzzel.nix
@@ -1,17 +1,26 @@
-{lib, ...}: {
+{
   programs.fuzzel = {
     enable = true;
     settings = {
       main = {
         dpi-aware = "no";
-        use-bold = "yes";
-        font = lib.mkForce "Ubuntu Nerd Font:size=20";
+        font = "Ubuntu Nerd Font:size=20";
         layer = "overlay";
         icon-theme = "Adwaita";
         image-size-ratio = "1";
       };
       colors = {
-        background = lib.mkForce "000000aa";
+        background = "#181926";
+        text = "cad3f5ff";
+        prompt = "b8c0e0ff";
+        placeholder = "8087a2ff";
+        input = "cad3f5ff";
+        match = "8aadf4ff";
+        selection = "5b6078ff";
+        selection-text = "cad3f5ff";
+        selection-match = "8aadf4ff";
+        counter = "8087a2ff";
+        border = "#ff3366";
       };
     };
   };
