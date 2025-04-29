97368ebb8cd5fb071f8aec5dc77cb12eecd17586
ian <ian.page38@gmail.com>
2025-04-29 15:14:01 +0200

    Update rofix.nix

--- home_manager/hyprland/rofix.nix
+++ home_manager/hyprland/rofix.nix
@@ -2,7 +2,7 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
-    theme = ./rofi/launcher.rasi; # optional
+    # theme = ./rofi/launcher.rasi; # optional
     extraConfig = {
       modi = "drun,window";
       show-icons = true;
