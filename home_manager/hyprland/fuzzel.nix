39a96fba54b204ca1741217e3621ca94314b87ce
ian <ian.page38@gmail.com>
2025-04-29 17:04:41 +0200

    Update fuzzel.nix

--- home_manager/hyprland/fuzzel.nix
+++ home_manager/hyprland/fuzzel.nix
@@ -1,3 +1,11 @@
 {
-  programs.fuzzel.enable = true;
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        "dpi-aware" = "no";
+        layer = "overlay";
+      };
+    };
+  };
 }
