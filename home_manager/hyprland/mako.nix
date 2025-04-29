345e2b1cf1db6f286903f968837efa35fdebedac
ian <ian.page38@gmail.com>
2025-04-29 20:28:44 +0200

    Update mako.nix

--- home_manager/hyprland/mako.nix
+++ home_manager/hyprland/mako.nix
@@ -8,10 +8,10 @@
     backgroundColor = "#181926";
     textColor = "#eed49f";
     borderColor = "#ffcc33";
-    borderRadius = 8;
+    borderRadius = "8";
 
     # Timeout in milliseconds (0 = no timeout)
-    defaultTimeout = 5000;
+    defaultTimeout = "5000";
 
     # Show notifications on a specific Wayland output
     output = "eDP-1";
