5d6bfef66af164c2f8a33c73b9d81267ff0f00e8
ian <ian.page38@gmail.com>
2025-04-30 09:27:56 +0200

    Update hyprland.nix

--- home_manager/hyprland/hyprland.nix
+++ home_manager/hyprland/hyprland.nix
@@ -133,7 +133,7 @@
         "$mainMod CONTROL,Q,exit"
         "$mainMod,P,pseudo"
         "$mainMod,F,exec,$fileManager"
-        "$mainMod,CMD,exec,$menu"
+        "$mainMod,SUPER,exec,$menu"
         "$mainMod,E,exec,$editor"
         "$mainMod,Z,togglesplit"
         "$mainMod,G,exec,$browser"
