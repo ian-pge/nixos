79c38bf590fbffb82687df1a4166350e251d4303
ian <ian.page38@gmail.com>
2025-05-01 02:15:13 +0200

    Update docker.nix

--- system/shared/docker.nix
+++ system/shared/docker.nix
@@ -3,6 +3,7 @@
 
   hardware.nvidia-container-toolkit.enable = true;
   hardware.nvidia-container-toolkit.mount-nvidia-docker-1-directories = true;
+  hardware.nvidia-container-toolkit.mount-nvidia-executables = true;
 
   virtualisation.docker = {
     enable = true;
