d9372ce65294e397905732855385a34b90ec943f
ian <ian.page38@gmail.com>
2025-05-02 13:46:26 +0200

    Update starship.nix

--- home_manager/hyprland/starship.nix
+++ home_manager/hyprland/starship.nix
@@ -85,7 +85,8 @@
         style = "fg:flamingo";
         read_only = " ";
         read_only_style = "fg:flamingo";
-        format = "[$read_only]($read_only_style)[$path]($style)[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)";
+        format = "[$read_only]($read_only_style)[$path]($style)";
+        repo_root_format = "[$read_only]($read_only_style)[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)[$path]($style)";
         before_repo_root_style = "fg:flamingo";
         repo_root_style = "fg:maroon";
       };
