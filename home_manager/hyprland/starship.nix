53155122d8c89fd50177ef8b81b65268c86ceeab
ian <ian.page38@gmail.com>
2025-05-02 14:05:29 +0200

    Update starship.nix

--- home_manager/hyprland/starship.nix
+++ home_manager/hyprland/starship.nix
@@ -40,7 +40,7 @@
 
       # ─ What gets printed on the left prompt line ─────────────────────────────
       format = ''
-        $os $username@$hostname $directory $git_branch$line_break$character
+        $os $username@$hostname $directory $git_branch$line_break$character$line_break
       '';
 
       # 1 • Current time (18:49) -------------------------------------------------
