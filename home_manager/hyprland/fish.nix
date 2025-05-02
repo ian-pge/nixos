799b67109bf9071d7f051bffc8b05909d2e3c43f
ian <ian.page38@gmail.com>
2025-05-02 16:23:07 +0200

    add the empty line

--- home_manager/hyprland/fish.nix
+++ home_manager/hyprland/fish.nix
@@ -4,9 +4,7 @@
     interactiveShellInit = ''
       set fish_greeting
       fish_vi_key_bindings
-      function prompt_newline --on-event fish_postexec
-        echo
-      end
+      functions -q prompt_newline; and prompt_newline >/dev/null
     '';
     plugins = [
       {
