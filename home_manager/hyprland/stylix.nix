bb8d9f41cc4f5c99f1aef87f3996cfd9d9b62c48
ian <ian.page38@gmail.com>
2025-05-02 11:14:45 +0200

    Update stylix.nix

--- home_manager/hyprland/stylix.nix
+++ home_manager/hyprland/stylix.nix
@@ -1,5 +1,7 @@
 {
-  stylix.targets.ghostty.enable = true;
-  stylix.targets.yazi.enable = true;
-  # stylix.targets.fuzzel.enable = true;
+  stylix.targets = {
+    ghostty.enable = true;
+    yazi.enable = true;
+    fish.enable = true;
+  };
 }
