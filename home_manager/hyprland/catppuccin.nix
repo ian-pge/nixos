3aca00df21eb2ab6bbd46615a2f6a05ca16d0581
ian <ian.page38@gmail.com>
2025-05-02 12:15:56 +0200

    Update catppuccin.nix

--- home_manager/hyprland/catppuccin.nix
+++ home_manager/hyprland/catppuccin.nix
@@ -8,9 +8,28 @@
       enable = true;
       flavor = "macchiato";
     };
+    bat = {
+      enable = true;
+      flavor = "macchiato";
+    };
     lazygit = {
       enable = true;
       flavor = "macchiato";
+    };
+    fzf = {
+      enable = true;
+      flavor = "macchiato";
+      accent = "sapphire";
+    };
+    yazi = {
+      enable = true;
+      flavor = "macchiato";
+      accent = "sapphire";
+    };
+    zed = {
+      enable = true;
+      flavor = "macchiato";
+      accent = "sapphire";
     };
   };
 }
