855312f0129fea1585d9a01177ac15807a41ed23
ian <ian.page38@gmail.com>
2025-05-02 14:30:27 +0200

    Update starship.nix

--- home_manager/hyprland/starship.nix
+++ home_manager/hyprland/starship.nix
@@ -2,7 +2,7 @@
   programs.fish.functions = {
     starship_transient_prompt_func.body = ''starship module time'';
     prompt_newline = {
-      onEvent = "fish_postexec";
+      onEvent = "fish_prompt";
       body = ''echo ""'';
     };
   };
