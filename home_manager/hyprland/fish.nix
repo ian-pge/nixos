702e7fc0c5077f2f3dd852499db59225466e0499
ian <ian.page38@gmail.com>
2025-05-04 19:17:10 +0200

    Update fish.nix

--- home_manager/hyprland/fish.nix
+++ home_manager/hyprland/fish.nix
@@ -5,8 +5,8 @@
       set fish_greeting
       fish_vi_key_bindings
       functions -q prompt_newline; and prompt_newline >/dev/null
-      bind -M default y fish_clipboard_copy
-      bind -M default p fish_clipboard_paste
+      bind y fish_clipboard_copy
+      bind p fish_clipboard_paste
     '';
     plugins = [
       {
