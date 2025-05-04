da15e4ea385f1dbd3fe34ca782eaf68acdc2d3d2
ian <ian.page38@gmail.com>
2025-05-04 19:26:54 +0200

    Update fish.nix

--- home_manager/hyprland/fish.nix
+++ home_manager/hyprland/fish.nix
@@ -6,7 +6,7 @@
       fish_vi_key_bindings
       functions -q prompt_newline; and prompt_newline >/dev/null
       bind yy fish_clipboard_copy
-      bind vy fish_clipboard_copy
+      bind -M visual y fish_clipboard_copy
       bind p fish_clipboard_paste
     '';
     plugins = [
