879045748d97f6c7a37215a3e4f683b858de1510
ian <ian.page38@gmail.com>
2025-05-06 09:44:56 +0200

    remove helix

--- home_manager/hyprland/helix.nix
+++ home_manager/hyprland/helix.nix
@@ -18,49 +18,7 @@
           select = "underline";
         };
       };
-
-      # pull in Vim-style key bindings
-      import = [
-        "./config.toml" # helix-vim default map :contentReference[oaicite:2]{index=2}
-      ];
     };
-
-    ### Language servers (optional but nice) -------------------------------
-    # languages = lib.mkIf pkgs.stdenv.isLinux {
-    #   # adjust per-OS if needed :contentReference[oaicite:3]{index=3}
-    #   language = [
-    #     {
-    #       name = "bash";
-    #       language-server.command = "${pkgs.bashLanguageServer}/bin/bash-language-server";
-    #     }
-    #     {
-    #       name = "fish";
-    #       language-server.command = "${pkgs.fish}/bin/fish";
-    #     }
-    #     {
-    #       name = "nix";
-    #       language-server.command = "${pkgs.nixd}/bin/nixd";
-    #     }
-    #     {
-    #       name = "python";
-    #       language-server.command = "${pkgs.pyright}/bin/pyright-langserver";
-    #       language-server.args = ["--stdio"];
-    #     }
-    #     {
-    #       name = "json";
-    #       language-server.command = "${pkgs.vscode-langservers-extracted}/bin/vscode-json-language-server";
-    #       language-server.args = ["--stdio"];
-    #     }
-    #     {
-    #       name = "c";
-    #       language-server.command = "${pkgs.clang-tools}/bin/clangd";
-    #     }
-    #     {
-    #       name = "cpp";
-    #       language-server.command = "${pkgs.clang-tools}/bin/clangd";
-    #     }
-    #   ];
-    # };
 
     ### Extra binary (optional) -------------------------------------------
     # package = pkgs.helix;  # uncomment to override with a pinned helix if desired :contentReference[oaicite:5]{index=5}
