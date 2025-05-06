23056cb8d369e591bf27e7a0019e64727e5762db
ian <ian.page38@gmail.com>
2025-05-06 10:45:58 +0200

    Update neovim.nix

--- home_manager/hyprland/neovim.nix
+++ home_manager/hyprland/neovim.nix
@@ -1,20 +1,8 @@
 {
   programs.neovim = {
     enable = true;
-    plugins = "neovimPlugins";
     extraLuaConfig = ''
-      require('lspconfig').nixd.setup {
-        cmd       = { "nixd" };
-        filetypes = { "nix" };
-        root_dir  = require('lspconfig').util.root_pattern(
-                      "flake.nix", "shell.nix", ".git"
-                    );
-        settings = {
-          nixd = {
-            nixpkgs = { expr = "import <nixpkgs> {}"; };
-          };
-        };
-      }
+      vim.opt.number = true
     '';
   };
 }
