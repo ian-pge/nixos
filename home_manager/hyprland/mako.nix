896b4095344f64dadbb6df2e1fc73e83a6765407
ian <ian.page38@gmail.com>
2025-05-06 12:50:33 +0200

    Update mako.nix

--- home_manager/hyprland/mako.nix
+++ home_manager/hyprland/mako.nix
@@ -2,21 +2,22 @@
   services.mako = {
     # Enable the mako notification daemon
     enable = true; #
+    settings = {
+      # Styling and behavior settings
+      font = "Ubuntu Nerd Font 15";
+      backgroundColor = "#181926";
+      textColor = "#eed49f";
+      borderColor = "#ffcc33";
+      borderRadius = 10;
+      borderSize = 3;
 
-    # Styling and behavior settings
-    font = "Ubuntu Nerd Font 15";
-    backgroundColor = "#181926";
-    textColor = "#eed49f";
-    borderColor = "#ffcc33";
-    borderRadius = 10;
-    borderSize = 3;
+      defaultTimeout = 10000;
+      ignoreTimeout = true;
 
-    defaultTimeout = 10000;
-    ignoreTimeout = true;
+      output = "eDP-1";
 
-    output = "eDP-1";
-
-    # Pango‐markup format string: title in blue bold, then body
-    format = "<span color=\"#eed49f\"><b>%s</b></span>\\n%b";
+      # Pango‐markup format string: title in blue bold, then body
+      format = "<span color=\"#eed49f\"><b>%s</b></span>\\n%b";
+    };
   };
 }
