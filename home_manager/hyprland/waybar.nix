2fbfa452d3249a5576f7e91fbba9b1d523c1fc31
ian <ian.page38@gmail.com>
2025-05-07 09:58:31 +0200

    Update waybar.nix

--- home_manager/hyprland/waybar.nix
+++ home_manager/hyprland/waybar.nix
@@ -116,7 +116,7 @@
           interval = 3600;
           tooltip = true;
           return-type = "json";
-          format = "{icon} {}";
+          format = "{icon}{}";
           format-icons = {
             "has-updates" = "";
             "updated" = "";
