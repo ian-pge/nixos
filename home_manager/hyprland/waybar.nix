48f3745a7b4e39699998aeb3b5f5b1e64f431e76
ian <ian.page38@gmail.com>
2025-05-07 12:22:35 +0200

    Update waybar.nix

--- home_manager/hyprland/waybar.nix
+++ home_manager/hyprland/waybar.nix
@@ -225,7 +225,7 @@
       src = fetchCrate {
         # pulls the exact crate published to crates.io
         inherit pname version;
-        hash = "sha256"; # first run with lib.fakeSha256, copy real hash
+        hash = ""; # first run with lib.fakeSha256, copy real hash
       };
 
       # Cargo.lock lives in the repo, so just grab the hashes once:
