5b5d5bade76abf3cd87e13631ac538c1ab5d8e8e
ian <ian.page38@gmail.com>
2025-05-08 14:51:23 +0200

    Update steam.nix

--- system/shared/steam.nix
+++ system/shared/steam.nix
@@ -1,6 +1,7 @@
 {
   programs.steam = {
     enable = true;
+    extraPkgs = pkgs: with pkgs.pkgsi686Linux; [networkmanager];
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
