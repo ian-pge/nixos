127b62ad7b9b49be32ff9dedae53e1a45d3ae299
ian <ian.page38@gmail.com>
2025-05-08 18:41:01 +0200

    Update hyprpanel.nix

--- home_manager/hyprland/hyprpanel.nix
+++ home_manager/hyprland/hyprpanel.nix
@@ -34,15 +34,15 @@
     # Configure bar layouts for monitors.
     # See 'https://hyprpanel.com/configuration/panel.html'.
     # Default: null
-    layout = {
-      bar.layouts = {
-        "0" = {
-          left = ["dashboard" "workspaces"];
-          middle = ["media"];
-          right = ["volume" "systray" "notifications"];
-        };
-      };
-    };
+    # layout = {
+    #   bar.layouts = {
+    #     "0" = {
+    #       left = ["dashboard" "workspaces"];
+    #       middle = ["media"];
+    #       right = ["volume" "systray" "notifications"];
+    #     };
+    #   };
+    # };
 
     # Configure and theme almost all options from the GUI.
     # Options that require '{}' or '[]' are not yet implemented,
