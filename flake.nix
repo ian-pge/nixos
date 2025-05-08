4af23b18bb5eef51a5c879587f2536495566ef3e
ian <ian.page38@gmail.com>
2025-05-08 19:23:40 +0200

    back to old flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,47 @@
 {
-  description = "My NixOS configuration";
+  description = "Nixos config flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    disko.url = "github:nix-community/disko";
-    impermanence.url = "github:nix-community/impermanence";
-    home-manager.url = "github:nix-community/home-manager";
-    catppuccin.url = "github:catppuccin/nix";
-    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    catppuccin = {
+      url = "github:catppuccin/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-utils,
-    disko,
-    impermanence,
-    home-manager,
-    catppuccin,
-    hyprpanel,
-    ...
-  }: {
-    # overlays visible to outside users (optional)
-    overlays.default = [
-      (import ./overlays/bambustudio.nix)
-      hyprpanel.overlay
-    ];
+  outputs = {nixpkgs, ...} @ inputs: let
+    # Expose the overlay so it can be reused elsewhere
+    overlays = {
+      bambustudio = import ./overlays/bambustudio.nix;
+    };
+  in {
+    # Make overlay usable from outside the flake (optional)
+    inherit overlays;
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      nixpkgs.overlays = self.overlays.default;
       specialArgs = {inherit inputs;};
-
       modules = [
+        ({...}: {
+          nixpkgs.overlays = [
+            overlays.bambustudio
+          ];
+        })
         ./system/specialisation.nix
       ];
     };
