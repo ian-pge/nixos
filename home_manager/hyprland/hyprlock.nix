6792aa12e0d5a7dd10eafbf3697ea4e7b58b166c
ian <ian.page38@gmail.com>
2025-05-12 12:08:45 +0200

    Update hyprlock.nix

--- home_manager/hyprland/hyprlock.nix
+++ home_manager/hyprland/hyprlock.nix
@@ -95,7 +95,7 @@
       label {
           monitor = eDP-1
           text = cmd[update:1000] echo "<b><big> $(date +'%H:%M') </big></b>"
-          color = rgba(33ccffee)
+          color = $sapphire
           font_size = 112
           font_family = Ubuntu Nerd Font
           shadow_passes = 3
@@ -108,7 +108,7 @@
       label {
           monitor = eDP-1
           text = cmd[update:18000000] echo "<b><big> $(date +'%A') </big></b>"
-          color = $text
+          color = $pink
           font_size = 50
           font_family = Ubuntu Nerd Font
           position = 0, -220
@@ -119,7 +119,7 @@
       label {
           monitor = eDP-1
           text = cmd[update:18000000] echo "<b> $(date +'%d %b') </b>"
-          color = $text
+          color = $pink
           font_size = 30
           font_family = Ubuntu Nerd Font
           position = 0, -280
@@ -130,7 +130,7 @@
       label {
           monitor = eDP-1
           text = cmd[update:100] echo "<b> <big> $(python3 -c 'import datetime,sys;birth=datetime.datetime(1998,11,15);print(f"{((datetime.datetime.now()-birth).total_seconds()/(365.2425*24*3600)):.9f}")') </big></b>"
-          color = $text
+          color = $yellow
           font_size = 20
           font_family = Ubuntu Nerd Font
           position = 0, 40
