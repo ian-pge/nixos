e0a9637f3b67b3a6ce37caecc5257995c0c30dd7
ian <ian.page38@gmail.com>
2025-05-12 10:18:04 +0200

    Update hyprlock.nix

--- home_manager/hyprland/hyprlock.nix
+++ home_manager/hyprland/hyprlock.nix
@@ -83,40 +83,40 @@
       }
 
       # ---------------- Labels ----------------
-      label {
-          monitor = eDP-1
-          text = cmd[update:1000] echo "<b><big> $(date +'%H:%M') </big></b>"
-          color = rgba(33ccffee)
-          font_size = 112
-          font_family = Ubuntu Nerd Font
-          shadow_passes = 3
-          shadow_size = 4
-          position = 0, -40
-          halign = center
-          valign = top
-      }
+      # label {
+      #     monitor = eDP-1
+      #     text = cmd[update:1000] echo "<b><big> $(date +'%H:%M') </big></b>"
+      #     color = rgba(33ccffee)
+      #     font_size = 112
+      #     font_family = Ubuntu Nerd Font
+      #     shadow_passes = 3
+      #     shadow_size = 4
+      #     position = 0, -40
+      #     halign = center
+      #     valign = top
+      # }
 
-      label {
-          monitor = eDP-1
-          text = cmd[update:18000000] echo "<b><big> $(date +'%A') </big></b>"
-          color = $text
-          font_size = 50
-          font_family = Ubuntu Nerd Font
-          position = 0, -220
-          halign = center
-          valign = top
-      }
+      # label {
+      #     monitor = eDP-1
+      #     text = cmd[update:18000000] echo "<b><big> $(date +'%A') </big></b>"
+      #     color = $text
+      #     font_size = 50
+      #     font_family = Ubuntu Nerd Font
+      #     position = 0, -220
+      #     halign = center
+      #     valign = top
+      # }
 
-      label {
-          monitor = eDP-1
-          text = cmd[update:18000000] echo "<b> $(date +'%d %b') </b>"
-          color = $text
-          font_size = 30
-          font_family = Ubuntu Nerd Font
-          position = 0, -280
-          halign = center
-          valign = top
-      }
+      # label {
+      #     monitor = eDP-1
+      #     text = cmd[update:18000000] echo "<b> $(date +'%d %b') </b>"
+      #     color = $text
+      #     font_size = 30
+      #     font_family = Ubuntu Nerd Font
+      #     position = 0, -280
+      #     halign = center
+      #     valign = top
+      # }
 
       # label {
       #     monitor = eDP-1
