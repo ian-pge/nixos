4fbb005616871bec648f6dbaca0977392f227971
ian <ian.page38@gmail.com>
2025-05-12 14:09:39 +0200

    Update fish.nix

--- home_manager/hyprland/fish.nix
+++ home_manager/hyprland/fish.nix
@@ -6,18 +6,10 @@
 
       functions -q prompt_newline; and prompt_newline >/dev/null
 
-      function __vim_put_after_clipboard
-        commandline -f forward-char
-        fish_clipboard_paste
-        commandline -f backward-char
-      end
-
-      fish_vi_key_bindings --no-erase
+      fish_vi_key_bindings
       bind yy fish_clipboard_copy
       bind -M visual y fish_clipboard_copy
-      # bind -M default P fish_clipboard_paste
-      bind --erase --preset -M default p
-      bind -M default p __vim_put_after_clipboard
+      bind -M default p fish_clipboard_paste
     '';
     plugins = [
       {
