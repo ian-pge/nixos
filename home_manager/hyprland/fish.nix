89743c713932f309bdc07a313e0bdb6df4d8d881
ian <ian.page38@gmail.com>
2025-05-12 14:51:52 +0200

    Update fish.nix

--- home_manager/hyprland/fish.nix
+++ home_manager/hyprland/fish.nix
@@ -9,8 +9,8 @@
       fish_vi_key_bindings
       bind yy fish_clipboard_copy
       bind -M visual y fish_clipboard_copy
-      bind -M default p forward-char-passive fish_clipboard_paste backward-char-passive
-      bind -M default P fish_clipboard_paste
+      # bind -M default p forward-char-passive fish_clipboard_paste backward-char-passive
+      # bind -M default P fish_clipboard_paste
     '';
     plugins = [
       {
