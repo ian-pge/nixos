33d2b5a448ef9c372cbeea956192a39f43acd69d
ian <ian.page38@gmail.com>
2025-05-12 13:29:28 +0200

    Update hyprlock.nix

--- home_manager/hyprland/hyprlock.nix
+++ home_manager/hyprland/hyprlock.nix
@@ -2,7 +2,11 @@
   programs.hyprlock = {
     enable = true;
 
+    # Make sure the $var palette appears before anything that references it
+    importantPrefixes = ["$"];
+
     settings = {
+      # --------------- Catppuccin Mocha palette ---------------
       "$rosewater" = "rgb(f4dbd6)";
       "$rosewaterAlpha" = "f4dbd6";
       "$flamingo" = "rgb(f0c6c6)";
@@ -56,6 +60,7 @@
       "$crust" = "rgb(181926)";
       "$crustAlpha" = "181926";
 
+      # ---------------- General options ----------------------
       general = {hide_cursor = true;};
 
       animations = {
@@ -63,6 +68,7 @@
         animation = "fadeIn, 1, 10, ease";
       };
 
+      # ---------------- Backgrounds --------------------------
       background = [
         {
           monitor = "eDP-1";
@@ -75,9 +81,21 @@
           vibrancy = 0.21;
           vibrancy_darkness = 0.0;
         }
-        {monitor = "HDMI-A-1";}
+        {
+          monitor = "HDMI-A-1";
+          # Uncomment and adjust these if you want a real image here
+          # path = "/etc/nixos/material/wallpaper.png";
+          # blur_size = 2;
+          # blur_passes = 3;
+          # noise = 0.0117;
+          # contrast = 1.3;
+          # brightness = 0.8;
+          # vibrancy = 0.21;
+          # vibrancy_darkness = 0.0;
+        }
       ];
 
+      # ---------------- Labels --------------------------------
       label = [
         {
           monitor = "eDP-1";
@@ -91,11 +109,42 @@
           halign = "center";
           valign = "top";
         }
-        # …Duplicate for each label block
+        {
+          monitor = "eDP-1";
+          text = ''cmd[update:18000000] echo "<b><big> $(date +'%A') </big></b>"'';
+          color = "$pink";
+          font_size = 50;
+          font_family = "Ubuntu Nerd Font";
+          position = "0, -210";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          monitor = "eDP-1";
+          text = ''cmd[update:18000000] echo "<b> $(date +'%d %b') </b>"'';
+          color = "$pink";
+          font_size = 30;
+          font_family = "Ubuntu Nerd Font";
+          position = "0, -280";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          monitor = "eDP-1";
+          text = ''
+            cmd[update:100] echo "<b> <big> $(python3 -c 'import datetime,sys;birth=datetime.datetime(1998,11,15);print(f\"{((datetime.datetime.now()-birth).total_seconds()/(365.2425*24*3600)):.9f}\")') </big></b>"
+          '';
+          color = "$yellow";
+          font_size = 20;
+          font_family = "Ubuntu Nerd Font";
+          position = "0, 40";
+          halign = "center";
+          valign = "bottom";
+        }
       ];
 
+      # ---------------- Input field ---------------------------
       "input-field" = {
-        # hypr syntax allows hyphens in section names
         monitor = "eDP-1";
         size = "250, 50";
         outline_thickness = 3;
