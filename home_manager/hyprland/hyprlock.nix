850d36d7ad4f77e3303849abe0b03daf42ab31c6
ian <ian.page38@gmail.com>
2025-05-12 10:58:58 +0200

    Update hyprlock.nix

--- home_manager/hyprland/hyprlock.nix
+++ home_manager/hyprland/hyprlock.nix
@@ -59,8 +59,8 @@
 
       animations {
         bezier = ease, 0.42, 0, 1, 1
-        animation = fadeOut, 1, 4, default
-        animation = fadeIn, 1, 10, default
+        animation = fade, 1, 4
+        animation = fade, 1, 10
       }
 
       # ---------------- Backgrounds ----------------
