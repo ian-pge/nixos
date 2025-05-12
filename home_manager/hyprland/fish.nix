ccf29681dbdc75ad8cf2fa65c0f1dbba07c433e4
ian <ian.page38@gmail.com>
2025-05-12 13:51:44 +0200

    Update fish.nix

--- home_manager/hyprland/fish.nix
+++ home_manager/hyprland/fish.nix
@@ -3,11 +3,21 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting
-      fish_vi_key_bindings
+
       functions -q prompt_newline; and prompt_newline >/dev/null
+
+      function __vim_put_after_clipboard \
+              --description 'Vim-style p: paste clipboard AFTER the cursor'
+          commandline -f forward-char           # move one cell right
+          fish_clipboard_paste                  # insert clipboard contents
+          commandline -f backward-char          # land cursor before the pasted text
+      end
+
+      fish_vi_key_bindings
       bind yy fish_clipboard_copy
       bind -M visual y fish_clipboard_copy
-      bind p fish_clipboard_paste
+      bind -M default P fish_clipboard_paste
+      bind -M default p '__vim_put_after_clipboard'
     '';
     plugins = [
       {
