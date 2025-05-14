4a5c50180a0ae4adcc792c01429eb4c4e8d48aba
ian <ian.page38@gmail.com>
2025-05-14 13:12:34 +0200

    Update zed.nix

--- home_manager/shared/zed.nix
+++ home_manager/shared/zed.nix
@@ -3,69 +3,69 @@
     enable = true;
 
     # This whole block is rendered to ~/.config/zed/settings.json
-    userSettings = {
-      assistant = {
-        default_model = {
-          provider = "anthropic";
-          model = "claude-3-7-sonnet-latest";
-        };
-        version = "2";
-      };
+    #   userSettings = {
+    #     assistant = {
+    #       default_model = {
+    #         provider = "anthropic";
+    #         model = "claude-3-7-sonnet-latest";
+    #       };
+    #       version = "2";
+    #     };
 
-      icon_theme = "Catppuccin Macchiato";
-      features = {
-        edit_prediction_provider = "zed";
-      };
-      restore_on_startup = "none";
+    #     icon_theme = "Catppuccin Macchiato";
+    #     features = {
+    #       edit_prediction_provider = "zed";
+    #     };
+    #     restore_on_startup = "none";
 
-      tab_size = 2;
+    #     tab_size = 2;
 
-      lsp = {
-        nil = {
-          binary = {path_lookup = true;};
-          initialization_options = {
-            formatting = {
-              command = ["alejandra"];
-            };
-          };
-        };
-      };
+    #     lsp = {
+    #       nil = {
+    #         binary = {path_lookup = true;};
+    #         initialization_options = {
+    #           formatting = {
+    #             command = ["alejandra"];
+    #           };
+    #         };
+    #       };
+    #     };
 
-      languages = {
-        Nix = {
-          format_on_save = "language_server";
-        };
-      };
+    #     languages = {
+    #       Nix = {
+    #         format_on_save = "language_server";
+    #       };
+    #     };
 
-      show_edit_predictions = true;
-      vim_mode = true;
-      vim = {
-        cursor_shape = {
-          normal = "block";
-          insert = "bar";
-          replace = "underline";
-          visual = "block";
-        };
-      };
+    #     show_edit_predictions = true;
+    #     vim_mode = true;
+    #     vim = {
+    #       cursor_shape = {
+    #         normal = "block";
+    #         insert = "bar";
+    #         replace = "underline";
+    #         visual = "block";
+    #       };
+    #     };
 
-      # theme = {
-      #   mode = "system";
-      #   dark = "Catppuccin Macchiato (sapphire)";
-      # };
+    #     # theme = {
+    #     #   mode = "system";
+    #     #   dark = "Catppuccin Macchiato (sapphire)";
+    #     # };
 
-      terminal = {
-        font_family = "Hack Nerd Font";
-        dock = "bottom";
-        font_size = 16;
-        blinking = "on";
-        line_height = "standard";
-      };
+    #     terminal = {
+    #       font_family = "Hack Nerd Font";
+    #       dock = "bottom";
+    #       font_size = 16;
+    #       blinking = "on";
+    #       line_height = "standard";
+    #     };
 
-      buffer_font_family = "Hack Nerd Font";
-      buffer_font_size = 16;
+    #     buffer_font_family = "Hack Nerd Font";
+    #     buffer_font_size = 16;
 
-      ui_font_family = "Ubuntu Nerd Font";
-      ui_font_size = 16;
-    };
+    #     ui_font_family = "Ubuntu Nerd Font";
+    #     ui_font_size = 16;
+    #   };
   };
 }
