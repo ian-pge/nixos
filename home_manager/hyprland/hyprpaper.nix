9b47b08d5a3d221662603417b9952fa5b2df8aa0
ian <ian.page38@gmail.com>
2025-05-16 16:23:17 +0200

    Update hyprpaper.nix

--- home_manager/hyprland/hyprpaper.nix
+++ home_manager/hyprland/hyprpaper.nix
@@ -1,10 +1,12 @@
 {
   services.hyprpaper = {
-    enable = true; # turn on the hyprpaper service
-    settings = {
-      ipc = "off"; # enable fast IPC mode for live changes
-      preload = ["${../../material/wallpaper.png}"]; # images to load at startup
-      wallpaper = ["${../../material/wallpaper.png}"]; # apply to all monitors
+    enable = true;
+    settings = let
+      wp = ../../material/wallpaper.png; # relative to this .nix file
+    in {
+      ipc = "off";
+      preload = ["${wp}"];
+      wallpaper = [",${wp}"]; # apply to all monitors
     };
   };
 }
