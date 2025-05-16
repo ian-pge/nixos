4964791902bab4e19f0a2f4fdbb2729147a4e4bc
ian <ian.page38@gmail.com>
2025-05-16 16:25:12 +0200

    Update hyprpaper.nix

--- home_manager/hyprland/hyprpaper.nix
+++ home_manager/hyprland/hyprpaper.nix
@@ -1,12 +1,8 @@
 {
   services.hyprpaper = {
     enable = true;
-    settings = let
-      wp = ../../material/wallpaper.png; # relative to this .nix file
-    in {
-      ipc = "off";
-      preload = ["${wp}"];
-      wallpaper = [",${wp}"]; # apply to all monitors
-    };
+    ipc = "off";
+    preload = ["${../../material/wallpaper.png}"];
+    wallpaper = [",${../../material/wallpaper.png}"]; # apply to all monitors
   };
 }
