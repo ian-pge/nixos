14bb9549b8d9673bd695834ea394d2fec5579430
ian <ian.page38@gmail.com>
2025-05-19 15:39:40 +0200

    Update bambustudio.nix

--- overlays/bambustudio.nix
+++ overlays/bambustudio.nix
@@ -1,11 +1,19 @@
 final: prev: {
   bambu-studio = prev.bambu-studio.overrideAttrs (old: {
-    # version = "v02.00.02.57";
+    version = "v02.00.03.54";
+    src = final.fetchFromGitHub {
+      owner = "bambulab";
+      repo = "BambuStudio";
+      rev = "v02.00.03.54";
+      hash = "sha256-DUrlmeH3XJmke6VOwC7HREONQMIhg3wFYw7QTndY2/Y=";
+    };
+
+    # version = "01.00.01.50";
     # src = final.fetchFromGitHub {
     #   owner = "bambulab";
     #   repo = "BambuStudio";
-    #   rev = "v02.00.02.57";
-    #   hash = "sha256-DUrlmeH3XJmke6VOwC7HREONQMIhg3wFYw7QTndY2/Y=";
+    #   rev = "v01.00.01.50";
+    #   hash = "sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=";
     # };
 
     nativeBuildInputs = (old.nativeBuildInputs or []) ++ [prev.makeWrapper];
