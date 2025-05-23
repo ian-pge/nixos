9ff25d7f67f0c1cf0dd2af2136af806ee6fa6d48
ian <ian.page38@gmail.com>
2025-05-23 23:05:09 +0200

    Update gtk.nix

--- home_manager/hyprland/gtk.nix
+++ home_manager/hyprland/gtk.nix
@@ -10,10 +10,10 @@
   # Configure GTK settings
   gtk = {
     enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
+    # theme = {
+    #   name = "Adwaita-dark";
+    #   package = pkgs.gnome-themes-extra;
+    # };
     iconTheme = {
       name = "Adwaita";
       package = pkgs.adwaita-icon-theme;
