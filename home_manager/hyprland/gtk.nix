7f9c77cd5984f3382fa226c8ab04c59f5e9c9b50
ian <ian.page38@gmail.com>
2025-05-24 19:04:51 +0200

    test

--- home_manager/hyprland/gtk.nix
+++ home_manager/hyprland/gtk.nix
@@ -1,27 +1,27 @@
-{
+{pkgs, ...}: {
   # Enable dconf to manage GNOME settings
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark"; # Preferred color scheme
-      # gtk-theme = "Adwaita-dark"; # Set GTK theme to Adwaita-dark
+      gtk-theme = "Adwaita-dark"; # Set GTK theme to Adwaita-dark
     };
   };
 
   # Configure GTK settings
   gtk = {
     enable = true;
-    # theme = {
-    #   name = "Adwaita-dark";
-    #   package = pkgs.gnome-themes-extra;
-    # };
-    # iconTheme = {
-    #   name = "Adwaita";
-    #   package = pkgs.adwaita-icon-theme;
-    # };
-    # cursorTheme = {
-    #   name = "catppuccin-macchiato-dark-cursors";
-    #   package = pkgs.catppuccin-cursors.macchiatoDark;
-    # };
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+    cursorTheme = {
+      name = "catppuccin-macchiato-dark-cursors";
+      package = pkgs.catppuccin-cursors.macchiatoDark;
+    };
 
     # Tells GTK 3 to prefer a dark theme variant
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
