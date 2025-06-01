8e8dfc3cddb022f07b359d292db519d0e419700a
ian <ian.page38@gmail.com>
2025-06-01 19:34:53 +0200

    Update gnome.nix

--- system/gnome/gnome.nix
+++ system/gnome/gnome.nix
@@ -1,10 +1,14 @@
 {
   programs.dconf.enable = true;
 
-  services.xserver = {
-    enable = true;
-    videoDrivers = ["nvidia"];
-    displayManager.gdm.enable = true;
+  services = {
     desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+    xserver = {
+      enable = true;
+      videoDrivers = ["nvidia"];
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
   };
 }
