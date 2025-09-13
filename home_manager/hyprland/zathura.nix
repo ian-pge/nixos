07f012238a31ca284b069aab18e37d4ab3c55eed
ian <ian.page38@gmail.com>
2025-09-13 16:29:58 +0200

    Update zathura.nix

--- home_manager/hyprland/zathura.nix
+++ home_manager/hyprland/zathura.nix
@@ -4,7 +4,10 @@
     extraConfig = ''
       set recolor false
       set default-bg "#24273a"
+      set default-fg "#cad3f5"
       set statusbar-bg "#24273a"
+      set statusbar-fg "#cad3f5"
+      set inputbar-bg "#24273a"
       set inputbar-fg "#cad3f5"
     '';
   };
