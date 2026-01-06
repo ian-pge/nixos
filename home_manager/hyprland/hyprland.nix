16dcef27faf8ab181e144e075b1bd26dc23e094a
ian <ian.page38@gmail.com>
2026-01-06 20:32:30 +0100

    Update hyprland.nix

--- home_manager/hyprland/hyprland.nix
+++ home_manager/hyprland/hyprland.nix
@@ -32,14 +32,14 @@
 
       ### STATIC WORKSPACES ###
       workspace = [
-        "1, monitor:eDP-1"
-        "2, monitor:eDP-1"
-        "3, monitor:eDP-1"
-        "4, monitor:eDP-1"
-        "5, monitor:DP-2"
-        "6, monitor:DP-2"
-        "7, monitor:DP-2"
-        "8, monitor:DP-2"
+        "5, monitor:eDP-1"
+        "6, monitor:eDP-1"
+        "7, monitor:eDP-1"
+        "8, monitor:eDP-1"
+        "1, monitor:DP-2"
+        "2, monitor:DP-2"
+        "3, monitor:DP-2"
+        "4, monitor:DP-2"
       ];
 
       ### ENVIRONMENT ###
